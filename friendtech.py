4003750725f05d50421aa0f08b8fae0b69bce686
Mahfq <135901046+Mahfq@users.noreply.github.com>
2023-08-21 17:13:12 +0200

    Add files via upload

---
+++ friendtech.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# Load the CSV file into a DataFrame
+csv_file_path = 'friendtech.csv'
+dataframe = pd.read_csv(csv_file_path)
+
+# Extract the 'address' and 'twitterUsername' columns
+colonne_address = dataframe['address']
+colonne_twitter = dataframe['twitterUsername']
+
+# Define the target Twitter username to search
+# Exemple: if the twitterUsername is @elonmusk so enter without the @
+twitter = "elonmusk"   
+
+for index, elem in enumerate(colonne_twitter):
+    if elem == twitter:
+        address = colonne_address[index]
+        print(f"The Twitter username '{twitter}' is associated with the address '{address}'.")
+    else: 
+        print(f"The Twitter username '{twitter}' was not found in the data.")  
